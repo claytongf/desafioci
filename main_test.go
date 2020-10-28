f069fb4b0e3c34b9ce5e436547d621737eab39fe
Clayton Figueiredo <claytongf@gmail.com>
2020-10-28 15:32:23 -0300

    Desafio K8S

--- main_test.go
+++ main_test.go
@@ -7,11 +7,11 @@ import (
 
 func TestGreeting(t *testing.T) {
 	got := greeting("Code.education Rocks!")
-	want := "Code.education Rocks!"
+	want := "<b>Code.education Rocks!</b>"
 
 	if got != want {
 		t.Errorf("greeting() \n got: %v \n want: %v", got, want)
 	} else {
-		fmt.Println(greeting("Code.education Rocks!"))
+		fmt.Println(got)
 	}
 }
